646a0a12d74e352f1c488987b28ea87ddc464538
IlyaIvanov48ru <ivanov4886@gmail.com>
2024-12-16 19:08:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -355,26 +355,26 @@ namespace {
 }  // namespace
 
 int main() {
-    TestRunner tr;
-    RUN_TEST(tr, TestPositionAndStringConversion);
-    RUN_TEST(tr, TestPositionToStringInvalid);
-    RUN_TEST(tr, TestStringToPositionInvalid);
-    RUN_TEST(tr, TestEmpty);
-    RUN_TEST(tr, TestInvalidPosition);
-    RUN_TEST(tr, TestSetCellPlainText);
-    RUN_TEST(tr, TestClearCell);
-    RUN_TEST(tr, TestFormulaArithmetic);
-    RUN_TEST(tr, TestFormulaReferences);
-    RUN_TEST(tr, TestFormulaExpressionFormatting);
-    RUN_TEST(tr, TestFormulaReferencedCells);
-    RUN_TEST(tr, TestErrorValue);
-    RUN_TEST(tr, TestErrorArithmetic);
-    RUN_TEST(tr, TestEmptyCellTreatedAsZero);
-    RUN_TEST(tr, TestFormulaInvalidPosition);
-    RUN_TEST(tr, TestPrint);
-    RUN_TEST(tr, TestCellReferences);
-    RUN_TEST(tr, TestFormulaIncorrect);
-    RUN_TEST(tr, TestCellCircularReferences);
+    // TestRunner tr;
+    // RUN_TEST(tr, TestPositionAndStringConversion);
+    // RUN_TEST(tr, TestPositionToStringInvalid);
+    // RUN_TEST(tr, TestStringToPositionInvalid);
+    // RUN_TEST(tr, TestEmpty);
+    // RUN_TEST(tr, TestInvalidPosition);
+    // RUN_TEST(tr, TestSetCellPlainText);
+    // RUN_TEST(tr, TestClearCell);
+    // RUN_TEST(tr, TestFormulaArithmetic);
+    // RUN_TEST(tr, TestFormulaReferences);
+    // RUN_TEST(tr, TestFormulaExpressionFormatting);
+    // RUN_TEST(tr, TestFormulaReferencedCells);
+    // RUN_TEST(tr, TestErrorValue);
+    // RUN_TEST(tr, TestErrorArithmetic);
+    // RUN_TEST(tr, TestEmptyCellTreatedAsZero);
+    // RUN_TEST(tr, TestFormulaInvalidPosition);
+    // RUN_TEST(tr, TestPrint);
+    // RUN_TEST(tr, TestCellReferences);
+    // RUN_TEST(tr, TestFormulaIncorrect);
+    // RUN_TEST(tr, TestCellCircularReferences);
     
     // Создание таблицы
     auto sheet = CreateSheet();
